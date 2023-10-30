d44ff345241b91c7702f71dbea7bf332042f8f83
amoz <amoz_c.p.t@hotmail.com>
2023-10-30 13:48:49 -0300

    Save results in file

--- glcm_processor.py
+++ glcm_processor.py
@@ -8,12 +8,17 @@ import os
 class GLCMProcessor:
     def __init__(self, image):
         self.imagens = image
+        self.caminho_do_arquivo = "resultado.txt"
 
     def calc_glcm(self):
+        result = []
+        print("Gerando resultados...")
         for i in range(len(self.imagens)):
             filename, image = self.imagens[i]
 
             letter = filename[0]
+
+            letter_index = ord(letter) - ord('A')
 
             g = graycomatrix(image, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], normed=True, symmetric=True)
             contrast = graycoprops(g, 'contrast')[0, 0]
@@ -22,4 +27,13 @@ class GLCMProcessor:
             asm = graycoprops(g, 'ASM')[0, 0]
             energy = graycoprops(g, 'energy')[0, 0]
 
-            print(f"{contrast} {dissimilarity} {homogeneity} {asm} {energy} {letter}")
+            result.append(f"{contrast} {dissimilarity} {homogeneity} {asm} {energy} {letter_index}")
+        print("Finalizado!")
+
+        with open(self.caminho_do_arquivo, 'w') as arquivo:
+            for line in result:
+                arquivo.write(line + '\n')
+
+        arquivo.close()
+
+        print(f"Arquivo '{self.caminho_do_arquivo}' criado e conteúdo salvo com sucesso.")
